b9d157192fabbd053059bbe8406af9eb6e6ca3ba
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2015-10-12 16:57:39 +0200

    Added column constants for Security Port Calls and Ship2Ship Activities.

--- src/AnNa.SpreadsheetParser.Interface/Sheets/SecurityPortCallsSheetSpecification.cs
+++ src/AnNa.SpreadsheetParser.Interface/Sheets/SecurityPortCallsSheetSpecification.cs
@@ -4,22 +4,32 @@ namespace AnNa.SpreadsheetParser.Interface.Sheets
 {
 	public class SecurityPortCallsSheetSpecification : SecuritySheetSpecification
 	{
+		public class Columns
+		{
+			public const string DateOfArrival = "Date_of_arrival";
+			public const string DateOfDeparture = "Date_of_departure";
+			public const string Port = "Port";
+			public const string PortFacility = "Port_facility";
+			public const string SecurityLevel = "Security_level";
+			public const string SpecialOrAdditionalSecurityMeasuresTakenByTheShip = "Special_or_additional_security_measures_taken_by_the_ship";
+		}
+
 		public override List<string> ColumnNames
 		{
 			get
 			{
 				return new List<string>
 				{
-					"Date_of_arrival",
-					"Date_of_departure",
-					"Port",
-					"Port_facility",
-					"Security_level",
-					"Special_or_additional_security_measures_taken_by_the_ship",
+					Columns.DateOfArrival,
+					Columns.DateOfDeparture,
+					Columns.Port,
+					Columns.PortFacility,
+					Columns.SecurityLevel,
+					Columns.SpecialOrAdditionalSecurityMeasuresTakenByTheShip,
 				};
 			}
 		}
 
-		public override int MaximumNumberOfRows { get { return 10; /* Infinite */ } }
+		public override int MaximumNumberOfRows { get { return 10; } }
 	}
 }
--- src/AnNa.SpreadsheetParser.Interface/Sheets/SecurityS2SActivitiesSheetSpecification.cs
+++ src/AnNa.SpreadsheetParser.Interface/Sheets/SecurityS2SActivitiesSheetSpecification.cs
@@ -5,24 +5,35 @@ namespace AnNa.SpreadsheetParser.Interface.Sheets
 {
 	public class SecurityS2SActivitiesSheetSpecification : SecuritySheetSpecification 
 	{
+		public class Columns
+		{
+			public const string DateFrom = "Date_from";
+			public const string DateTo = "Date_to";
+			public const string Location = "*Location";
+			public const string Latitude = "Latitude";
+			public const string Longtitude = "Longtitude";
+			public const string ShipToShipActivitty = "Ship_to_ship_activitty";
+			public const string SecurityMeasuresAppliedInLieu = "Security_measures_applied_in_lieu";
+		}
+
 		public override List<string> ColumnNames
 		{
 			get
 			{
 				return new List<string>
 				{
-					"Date_from",
-					"Date_to",
-					"*Location",
-					"Latitude",
-					"Longtitude",
-					"Ship_to_ship_activitty",
-					"Security_measures_applied_in_lieu"
+					Columns.DateFrom,
+					Columns.DateTo,
+					Columns.Location,
+					Columns.Latitude,
+					Columns.Longtitude,
+					Columns.ShipToShipActivitty,
+					Columns.SecurityMeasuresAppliedInLieu
 				};
 			}
 		}
 
-		public override int MaximumNumberOfRows { get { return 14; /* Infinite */ } }
+		public override int MaximumNumberOfRows { get { return 14; } }
 
 	}
 }
