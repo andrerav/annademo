1c0124148a9b68692c1aa85a18d11164fdbdfce5
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2016-01-06 10:58:32 +0100

    Updated version numbers.

--- src/AnNa.SpreadsheetParser.EPPlus/Properties/AssemblyInfo.cs
+++ src/AnNa.SpreadsheetParser.EPPlus/Properties/AssemblyInfo.cs
@@ -1,13 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
 [assembly: AssemblyTitle("AnNa.SpreadsheetParser.EPPlus")]
 [assembly: AssemblyDescription("Advanced National Networks for Administrations")]
 [assembly: AssemblyCompany("Andreas Ravnestad")]
 [assembly: AssemblyProduct("AnNa.SpreadsheetParser.EPPlus")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a7b953b4-ac6b-4d76-b573-29ebf85409a5")]
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("1.1.*")]
