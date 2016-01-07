4b450ccd4a562d8437afdc06ce977ecea2d52257
Mats Hoel <mats.hoel@gmail.com>
2016-01-07 19:19:11 +0100

    Waste amounts made nullable Renamed WasteSheet class to WasteSheet10

--- src/AnNa.SpreadsheetParser.Interface/Sheets/Typed/WasteSheet.cs
+++ src/AnNa.SpreadsheetParser.Interface/Sheets/Typed/WasteSheet.cs
@@ -16,19 +16,19 @@ namespace AnNa.SpreadsheetParser.Interface.Sheets.Typed
 		public string Waste_Type_Description;
 
 		[Column("Amount_To_Be_Delivered", FriendlyName = "Amount to be Delivered")]
-		public double Amount_To_Be_Delivered;
+		public double? Amount_To_Be_Delivered;
 
 		[Column("Maximum_Dedicated_Storage_Capacity", FriendlyName = "Maximum Dedicated Storage Capacity")]
-		public double Maximum_Dedicated_Storage_Capacity;
+		public double? Maximum_Dedicated_Storage_Capacity;
 
 		[Column("Amount_Retained_On_Board", FriendlyName = "Amount Retained on Board")]
-		public double Amount_Retained_On_Board;
+		public double? Amount_Retained_On_Board;
 
 		[Column("Port_Of_Delivery_Of_Remaining_Waste", FriendlyName = "Port of Delivery of Remaining Waste")]
 		public string Port_Of_Delivery_Of_Remaining_Waste;
 
 		[Column("Estimated_Amount_Of_Waste_To_Be_Generated", FriendlyName = "Estimated Amount of Waste to be Generated")]
-		public double Estimated_Amount_Of_Waste_To_Be_Generated;
+		public double? Estimated_Amount_Of_Waste_To_Be_Generated;
 
 		[Column("Date_Of_Delivery_Of_Remaining_Waste", FriendlyName = "Date of Delivery of Remaining Waste")]
 		public DateTime? Date_Of_Delivery_Of_Remaining_Waste;
@@ -37,14 +37,14 @@ namespace AnNa.SpreadsheetParser.Interface.Sheets.Typed
 	[Serializable]
 	[SheetVersion(1, 0)]
 	[Deprecated]
-	public class WasteSheet : AbstractSheet<WasteSheetRow>
+	public class WasteSheet10 : AbstractSheet<WasteSheetRow>
 	{
 		public override string SheetName => "Waste_And_Residues";
 	}
 
 	[Serializable]
 	[SheetVersion(1, 1)]
-	public class WasheSheet11:WasteSheet
+	public class WasheSheet11:WasteSheet10
 	{
 		[Field("B4", FriendlyName = "Waste to be Delivered")]
 		public string WasteToBeDeliveredCell;
