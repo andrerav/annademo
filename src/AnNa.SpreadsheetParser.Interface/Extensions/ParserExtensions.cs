2bb88544c888dd2b93ea3fa9c2dfe01a6bb4b31d
Mats Hoel <mats.hoel@gmail.com>
2016-01-26 14:37:03 +0100

    Removed TryGetWorkbookVersion call from ParseWorkbook

--- src/AnNa.SpreadsheetParser.Interface/Extensions/ParserExtensions.cs
+++ src/AnNa.SpreadsheetParser.Interface/Extensions/ParserExtensions.cs
@@ -9,14 +9,11 @@ namespace AnNa.SpreadsheetParser.Interface.Extensions
 
 	public static class ParserExtensions
 	{
-		public static Dictionary<Type, object> ParseWorkbook(this IAnNaSpreadSheetParser10 parser, out Version workbookVersion, out string authority)
+		public static Dictionary<Type, object> ParseWorkbook(this IAnNaSpreadSheetParser10 parser, Version workbookVersion, string authority)
 		{
 			var result = new Dictionary<Type, object>();
 
 			parser.ThrowExceptionIfNotInitialized();
-
-			if (!parser.TryGetWorkbookVersion(out workbookVersion, out authority))
-				throw new InvalidWorkbookVersionException();
 
 			var sheetDefinitionGroups = ReflectionHelpers.GetSheetDefinitionsOrderedByAuthority(authority);
 
